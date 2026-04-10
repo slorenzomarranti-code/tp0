ee8e30bf94849af6220bfa75e8273cfd87d435f3
Lorenzo Marranti Santiago Nicolás <slorenzomarranti@frba.utn.edu.ar>
2026-04-10 20:43:16 +0000

    Modifico error del client.c y utils.c

--- client/src/utils.c
+++ client/src/utils.c
@@ -19,24 +19,37 @@ void* serializar_paquete(t_paquete* paquete, int bytes)
 int crear_conexion(char *ip, char* puerto)
 {
 	struct addrinfo hints;
-	struct addrinfo *server_info;
+    struct addrinfo *server_info;
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(ip, puerto, &hints, &server_info);
+    getaddrinfo(ip, puerto, &hints, &server_info);
 
-	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+    int socket_cliente = socket(server_info->ai_family,
+                                server_info->ai_socktype,
+                                server_info->ai_protocol);
 
-	// Ahora que tenemos el socket, vamos a conectarlo
+    if (socket_cliente == -1) {
+        printf("Error: No se pudo crear el socket del cliente.\n");
+        freeaddrinfo(server_info);
+        return -1;
+    }
 
+    int err = connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
 
-	freeaddrinfo(server_info);
+    if (err == -1) {
+        printf("Error: No se pudo conectar al servidor (connect fallo).\n");
+        freeaddrinfo(server_info);
+        return -1;
+    }
 
-	return socket_cliente;
+    freeaddrinfo(server_info);
+
+    return socket_cliente;
+
 }
 
 void enviar_mensaje(char* mensaje, int socket_cliente)
